e811560546dbff478941fc8dc7da63c6e9cbab66
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-03-25 13:38:34 -0300

    Objeto

---
+++ objetoLiteral.js
@@ -0,0 +1,13 @@
+const user = {
+  nome: "Letícia Nascimento",
+  email: "let.lu.nascimento@gmail.com",
+  nascimento: "2006-07-06",
+  role: "estudante",
+  ativo: true,
+
+  exibirInfos: function(){
+    console.log(this.nome, this.email)
+  }
+};
+
+//user.exibirInfos();
